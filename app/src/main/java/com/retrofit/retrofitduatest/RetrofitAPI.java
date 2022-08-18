44105c9f2f3e70d6eca688c5e8678938ea8fe8de
SontagBae <hendar.sontag@gmail.com>
2022-08-18 12:54:23 +0700

    Initial commit

---
+++ app/src/main/java/com/retrofit/retrofitduatest/RetrofitAPI.java
@@ -0,0 +1,16 @@
+package com.retrofit.retrofitduatest;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RetrofitAPI {
+
+    // as we are making a post request to post a data
+    // so we are annotating it with post
+    // and along with that we are passing a parameter as users
+    @POST("users")
+
+    //on below line we are creating a method to post our data.
+    Call<DataModal> createPost(@Body DataModal dataModal);
+}
